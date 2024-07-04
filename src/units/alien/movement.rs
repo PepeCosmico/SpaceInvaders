889c7d3e9955833bb3bd2b97727fd52b058c1b90
pepecosmico <pericollinas@gmail.com>
2024-07-04 16:47:23 +0200

    Aliens change direction

--- src/units/alien/movement.rs
+++ src/units/alien/movement.rs
@@ -1,20 +1,41 @@
 use bevy::prelude::*;
 
-use crate::GameStates;
+use crate::{fisics::Hitbox, GameStates};
 
 use super::Alien;
 
 #[derive(Resource)]
-struct AlienMovementTurn {
+struct AlienMovement {
     pub aliens: Vec<u8>,
     turn: u8,
+    direction: Direction,
 }
 
-impl AlienMovementTurn {
+enum Direction {
+    Left,
+    Right,
+}
+impl Direction {
+    fn get_num(&self) -> f32 {
+        match self {
+            Direction::Right => 1.0,
+            Direction::Left => -1.0,
+        }
+    }
+    fn change(&mut self) {
+        *self = match self {
+            Direction::Right => Direction::Left,
+            Direction::Left => Direction::Right,
+        }
+    }
+}
+
+impl AlienMovement {
     pub fn new() -> Self {
         Self {
             aliens: (0..55).collect(),
             turn: 0,
+            direction: Direction::Right,
         }
     }
 }
@@ -23,18 +44,25 @@ pub struct MovementPlugin;
 
 impl Plugin for MovementPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(AlienMovementTurn::new())
+        app.insert_resource(AlienMovement::new())
             .add_systems(Update, move_alien.run_if(in_state(GameStates::InGame)));
     }
 }
 
 fn move_alien(
-    mut alien_turn: ResMut<AlienMovementTurn>,
-    mut aliens_query: Query<(&mut Transform, &Alien)>,
+    mut alien_turn: ResMut<AlienMovement>,
+    mut aliens_query: Query<(&mut Transform, &Alien, &Hitbox)>,
 ) {
-    for (mut transform, alien) in aliens_query.iter_mut() {
+    for (mut transform, alien, hitbox) in aliens_query.iter_mut() {
         if alien.id == *alien_turn.aliens.get(alien_turn.turn as usize).unwrap() {
-            transform.translation.x += 4.0;
+            transform.translation.x += 4.0 * alien_turn.direction.get_num();
+            if transform.translation.x > 384.0 - hitbox.rect.x {
+                alien_turn.direction.change();
+                transform.translation.x = 384.0 - hitbox.rect.x;
+            } else if transform.translation.x > -384.0 + hitbox.rect.x {
+                alien_turn.direction.change();
+                transform.translation.x = -384.0 + hitbox.rect.x;
+            }
             break;
         }
     }
